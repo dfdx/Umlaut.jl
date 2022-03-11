ca68c5b243f41f34557b1828ce277ae8657ceb49
Andrei Zhabinski <andrei.zhabinski@gmail.com>
2022-03-12 01:56:14 +0300

    Replace broadcasted with broadcast in BcastCtx

--- src/contexts.jl
+++ src/contexts.jl
@@ -1,7 +1,5 @@
 # A couple of useful context types
 
-const broadcasted = Broadcast.broadcasted
-
 struct BcastCtx
     inner
 end
@@ -10,7 +8,7 @@ end
 function record_or_recurse!(t::Tracer{BcastCtx}, vs...)
     fvals = [v isa V ? t.tape[v].val : v for v in vs]
     return if isprimitive(t.tape.c.inner, fvals...)
-        push!(t.tape, mkcall(broadcasted, vs...))
+        push!(t.tape, mkcall(broadcast, vs...))
     else
         types = map(eltype, fvals[2:end])
         trace!(t, code_info_of(fvals[1], types...), vs...)
