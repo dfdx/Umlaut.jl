e96c4ccac297120a6136e1f8a4fc648acafa99d0
Andrei Zhabinski <azbs@toloka.ai>
2022-08-01 01:06:28 +0300

    Comment out BcastCtx

--- src/contexts.jl
+++ src/contexts.jl
@@ -1,17 +1,17 @@
-# A couple of useful context types
+# # A couple of useful context types
 
-struct BcastCtx
-    inner
-end
+# struct BcastCtx
+#     inner
+# end
 
 
-function trace_call!(t::Tracer{BcastCtx}, vs...)
-    fvals = [v isa V ? t.tape[v].val : v for v in vs]
-    return if isprimitive(t.tape.c.inner, fvals...)
-        push!(t.tape, mkcall(broadcast, vs...))
-    else
-        types = map(eltype, fvals[2:end])
-        # TODO: handle varargs
-        trace!(t, getcode(fvals[1], types), vs...)
-    end
-end
+# function trace_call!(t::Tracer{BcastCtx}, vs...)
+#     fvals = [v isa V ? t.tape[v].val : v for v in vs]
+#     return if isprimitive(t.tape.c.inner, fvals...)
+#         push!(t.tape, mkcall(broadcast, vs...))
+#     else
+#         types = map(eltype, fvals[2:end])
+#         # TODO: handle varargs
+#         trace!(t, getcode(fvals[1], types), vs...)
+#     end
+# end
